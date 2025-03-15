1b118dc53a6e21bfbe1b7573b9c8bcc1145d12a2
unknown <yanpbc@gmail.com>
2025-03-15 19:34:38 -0300

    atualizacao de espacos.index

--- resources/views/espacos.blade.php
+++ resources/views/espacos.blade.php
@@ -1,33 +1,55 @@
 @extends('layouts.basico')
 
 @section('content')
-<div class="bg-body-tertiary rounded">
+<div class="container bg-body-tertiary rounded">
     <div class="my-3 p-3 bg-body rounded shadow-sm">
-        <h6 class="border-bottom pb-2 mb-0">Espacos</h6>
-        <div class="d-flex text-body-secondary pt-3">
-        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        <p class="pb-3 mb-0 small lh-sm border-bottom">
-            <strong class="d-block text-gray-dark">@username</strong>
-            Some representative placeholder content, with some information about this user. Imagine this being some sort of status update, perhaps?
-        </p>
+        <div class="row">
+            <h6 class="border-bottom pb-2 mb-0">
+                <a class="link-body-emphasis link-underline link-underline-opacity-0" href="#">
+                    <strong>Novo Espaço</strong>
+                </a>
+            </h6>
         </div>
-        <div class="d-flex text-body-secondary pt-3">
-        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
-        <p class="pb-3 mb-0 small lh-sm border-bottom">
-            <strong class="d-block text-gray-dark">@username</strong>
-            Some more representative placeholder content, related to this other user. Another status update, perhaps.
-        </p>
+        <div class="row">
+            <div class="d-flex pt-3">
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+                <a href="#"class="link-secondary link-underline link-underline-opacity-0 small  mb-0 lh-sm">
+                    <span class="link-light">Espaço 01</span>
+                    <div class="d-flex justify-content-between">
+                        <p class="p2 me-5 ">teste com texto maior lalala lal ala lal a</p>
+                        <p class="p2 me-5">este com  com texto maior lalala lal ala lal</p>
+                        <p class="p2 me-5">este com texto maior lalala lal ala l </p></div>
+                    
+                </a>
+            </div>
         </div>
-        <div class="d-flex text-body-secondary pt-3">
-        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"></rect><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
-        <p class="pb-3 mb-0 small lh-sm border-bottom">
-            <strong class="d-block text-gray-dark">@username</strong>
-            This user also gets some representative placeholder content. Maybe they did something interesting, and you really want to highlight this in the recent updates.
-        </p>
+        <div class="row">
+            <div class="d-flex text-body-secondary pt-3">
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+                <a href="#"class="link-secondary link-underline link-underline-opacity-0 small  mb-0 lh-sm">
+                    <span class="link-light">Espaço 01</span>
+                    <div class="d-flex justify-content-between">
+                        <p class="p2 me-5 ">teste com texto maior lalala lal ala lal a</p>
+                        <p class="p2 me-5">este com  com texto maior lalala lal ala lal</p>
+                        <p class="p2 me-5">este com texto maior lalala lal ala l </p></div>
+                    
+                </a>
+            </div>
         </div>
-        <small class="d-block text-end mt-3">
-        <a href="#">All updates</a>
-        </small>
+        <div class="row">
+            <div class="d-flex text-body-secondary pt-3">
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+                <a href="#"class="link-secondary link-underline link-underline-opacity-0 small  mb-0 lh-sm">
+                    <span class="link-light">Espaço 01</span>
+                    <div class="d-flex justify-content-between">
+                        <p class="p2 me-5 ">teste com texto maior lalala lal ala lal a</p>
+                        <p class="p2 me-5">este com  com texto maior lalala lal ala lal</p>
+                        <p class="p2 me-5">este com texto maior lalala lal ala l </p></div>
+                    
+                </a>
+            </div>
+        </div>
+        
     </div>
 </div>
 @endsection
